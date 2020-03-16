89ece9ed5d0496be2f7f42a3a7b0393c0e5b0419
mixalis-tsino <61464993+mixalis-tsino@users.noreply.github.com>
2020-03-16 20:53:36 +0200

    Add files via upload

--- VelocityTrianglesApp.py
+++ VelocityTrianglesApp.py
@@ -13,85 +13,40 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
 
+pe = None
+l = None
 class VelocityTriangles(Screen):
-	p= ObjectProperty(None)  
-	f= ObjectProperty(None)
-	Rn= ObjectProperty(None)
-	a1= ObjectProperty(None)
-	a2= ObjectProperty(None)
-	a3= ObjectProperty(None)
-	b1= ObjectProperty(None)
-	b2= ObjectProperty(None)
-	b3= ObjectProperty(None)
-	
-	def angles(self):
-		
-		pe = self.p.text
-		s = str(pe)
-		l = Label(text = s,pos_hint={"x":-0.1,"y":0.1})
-		self.add_widget(l)
+    global p
+    p = ObjectProperty(None)
+    def some(self):
+        global pe
+        pe = str(self.p.text)
+        global l
+        # l = Label(text=pe, pos_hint={'x': - 0.3, 'y': -0.3})
 
-		# if pe != '':
-		# 	pe = float(pe)
-	 #    else:
-	 #    	pe = ''
-
-	 #    if fe != '':
-	 #        fe = float(fe)
-	 #    else:
-	 #        fe = ''
-
-	 #    if Rne != '':
-	 #        Rne = float(Rne)
-	 #    else:
-	 #        Rne = ''
-
-	 #    if a1e != '':
-	 #        a1e = float(a1e)
-	 #    else:
-	 #        a1e = ''
-
-	 #    if a2e != '':
-	 #         a2e= float(a2e)
-	 #    else:
-	 #        a2e = ''
-
-	 #    if b1e != '':
-	 #         b1e= float(b1e)
-	 #    else:
-	 #        b1e = ''
-
-	 #    if b2e != '':
-	 #         b2e= float(b2e)
-	 #    else:
-	 #        b2e = ''
-
-
-
-		
-
-    
 
 class NewWindow(Screen):
-	VelocityTriangles().angles()
+    def some1(self):
+        ke = pe
+        lk = Label(text= ke)
+        self.add_widget(lk)
+
 
 
 
 class WindowManager(ScreenManager):
     pass
 
+
 Config.set('graphics', 'resizable', True)
 Window.size = (600, 700)
 
 
 kv = Builder.load_file("VelocityTrianglesApp.kv")
-
 class VelocityTrianglesApp(App):
     def build(self):
         return kv
 
-
-
-
 if __name__ == '__main__':
-	VelocityTrianglesApp().run()
+	va = VelocityTrianglesApp()
+	va.run()
