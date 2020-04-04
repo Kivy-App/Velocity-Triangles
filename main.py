caa5999f2b0f01f46e4b9b7a2b5f2887b2a6a4b3
mixalis-tsino <61464993+mixalis-tsino@users.noreply.github.com>
2020-04-04 20:55:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+import sympy as sy
+
 
 class P(BoxLayout):
     pass
@@ -49,10 +51,154 @@ class VelocityTriangles(Screen):
             b2e = str(self.b2.text)
             b3e = str(self.b3.text)
 
-            a1e = str(round(-np.degrees(np.arctan(-((float(pe) / 2) - 1 + float(rne)) / float(fe))), 3))
-            a2e = str(round(np.degrees(np.arctan(((float(pe) / 2)+1-float(rne)) / float(fe))),3))
-            b1e = str(round(np.degrees(np.arctan(((float(pe) / 2) + float(rne)) / float(fe))),3))
-            b2e = str(round(-np.degrees(np.arctan(-((float(pe) / 2) - float(rne)) / float(fe))),3))
+
+            if pe == '':
+                pe = sy.symbols('pe')
+            else:
+                pe  = str(self.p.text)
+
+            if fe == '':
+                fe = sy.symbols('fe')
+            else:
+                fe  = str(self.f.text)
+
+
+            if rne == '':
+                rne = sy.symbols('rne')
+            else:
+                rne  = str(self.rn.text)
+
+            if a1e == '':
+                a1e = sy.symbols('a1e')
+            else:
+                a1e  = str(self.a1.text)
+
+            if a2e == '':
+                a2e = sy.symbols('a2e')
+            else:
+                a2e  = str(self.a2.text)
+
+            if b1e == '':
+                b1e = sy.symbols('b1e')
+            else:
+                b1e  = str(self.b1.text)
+
+            if b2e == '':
+                b2e = sy.symbols('b2e')
+            else:
+                b2e  = str(self.b2.text)
+
+            X = pe,fe,rne,a1e,a2e,b1e,b2e
+
+
+            X=list(X)
+
+
+            i=0
+
+            if str(X[i])!= 'pe':
+                del X[i]
+                pe = float(pe)
+            else:
+                X[i]== 'pe'
+
+                i=i+1
+
+
+            if str(X[i])!='fe':
+                del X[i]
+                fe = float(fe)
+            else:
+                X[i]=='fe'
+                i=i+1
+
+            if str(X[i])!='rne':
+                del X[i]
+                rne = float(rne)
+            else:
+                X[i]=='rne'
+                i=i+1
+
+            if str(X[i])!= 'a1e':
+                del X[i]
+                a1e = float(a1e)
+            else:
+                X[i]=='a1e'
+                i=i+1
+
+
+            if str(X[i])!='a2e':
+                del X[i]
+                a2e = float(a2e)
+            else:
+                X[i]=='a2e'
+                i=i+1
+
+            if str(X[i])!='b1e':
+                del X[i]
+                b1e = float(b1e)
+            else:
+                X[i]=='b1e'
+                i=i+1
+
+
+            if str(X[i])!='b2e':
+                del X[i]
+                b2e = float(b2e)
+            else:
+                X[i]!='b2e'
+
+
+
+            sys = sy.nsolve((a1e + 57.2955*(sy.atan(-((pe / 2) - 1 + rne) / fe)),
+                a2e- 57.2955*(sy.atan(((pe / 2)+1-rne) / fe)),
+                b1e - 57.2955*(sy.atan(((pe / 2) + rne) / fe)),
+                b2e + 57.2955*(sy.atan(-((pe / 2) - rne) / fe))),X,(1,1,1,1))
+
+            print(sys)
+            r=0
+            if str(self.p.text)=='':
+                pe = str(round(sys[r],3))
+                r=r+1
+            else :
+                pe = str(self.p.text)
+
+            if str(self.f.text)=='':
+                fe = str(round(sys[r],3))
+                r=r+1
+            else :
+                fe = str(self.f.text)
+
+            if str(self.rn.text)=='':
+                rne = str(round(sys[r],3))
+                r=r+1
+            else :
+                rne = str(self.rn.text)
+
+            if str(self.a1.text)=='':
+                a1e = str(round(sys[r],3))
+                r=r+1
+            else :
+                a1e = str(self.a1.text)
+
+            if str(self.a2.text)=='':
+                a2e = str(round(sys[r],3))
+                r=r+1
+            else :
+                a2e = str(self.a2.text)
+
+            if str(self.b1.text)=='':
+                b1e = str(round(sys[r],3))
+                r=r+1
+            else :
+                b1e = str(self.b1.text)
+
+            if str(self.b2.text)=='':
+                b2e = str(round(sys[r],3))
+                r=r+1
+            else :
+                b2e = str(self.b2.text)
+
 
             self.manager.get_screen('new').pText = pe
             self.manager.get_screen('new').fText = fe
@@ -64,7 +210,7 @@ class VelocityTriangles(Screen):
             self.manager.get_screen('new').b2Text = b2e
             self.manager.get_screen('new').b3Text = b3e
         except:
-            self.popup = firstPopup()
+             self.popup = firstPopup()
 
     def fontsize(self, text):
         if Window.size[0]>400:
@@ -142,6 +288,7 @@ class WindowManager(ScreenManager):
 
 Config.set('graphics', 'resizable', True)
 Window.size = (400, 700)
+
 
 kv = Builder.load_file("VelocityTrianglesApp.kv")
 
